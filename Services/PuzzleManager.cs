1854d3868174ba77dbf1335c5ad1154e7818b7a0
Seraphania <strings333@hotmail.com>
2026-03-19 16:35:49 +0800

    wip: puzzle manager service

---
+++ Services/PuzzleManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperSudoku.Models;
+
+namespace SuperSudoku.Services
+{
+	internal class PuzzleManager
+	{
+		private PuzzleBox _PuzzleBox;
+		private readonly string SuperSudokuPath = Path.Combine(
+			FileSystem.AppDataDirectory,
+			"SuperSudoku"
+		);
+
+
+		public PuzzleManager()
+		{
+			PuzzleBox puzzleBox = _PuzzleBox;
+
+			Directory.CreateDirectory(SuperSudokuPath);
+		}
+
+		public void LoadOrRequestPuzzleBox()
+		{
+			string path = SuperSudokuPath + "puzzles";
+			PuzzleBox puzzleBox = JsonWrangler.Load<PuzzleBox>(path);
+			if (puzzleBox != null)
+			{
+				_PuzzleBox = puzzleBox;
+			}
+			if (puzzleBox.
+		}
+
+
+		private class PuzzleBox
+		{
+			private List<Puzzle> EasyPuzzles;
+			private List<Puzzle> MediumPuzzles;
+			private List<Puzzle> HardPuzzles;
+			private Puzzle CurrentPuzzle;
+
+			public PuzzleBox(List<Puzzle> easyPuzzles, List<Puzzle> mediumPuzzles, List<Puzzle> hardPuzzles, Puzzle currentPuzzle)
+			{
+				this.EasyPuzzles = easyPuzzles;
+				this.MediumPuzzles = mediumPuzzles;
+				this.HardPuzzles = hardPuzzles;
+				this.CurrentPuzzle = currentPuzzle;
+			}
+		}
+	}
+
+
+}
